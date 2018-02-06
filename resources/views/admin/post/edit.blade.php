326a528e5252543b23745fefa45ec01d24a21947
sangwondee <sangwondee@gmail.com>
2018-02-06 23:44:11 +0700

    #10 Finish
    Laravel - Create Blog and Admin Panel | CRUD for Category and Tags

---
+++ resources/views/admin/post/edit.blade.php
@@ -0,0 +1,114 @@
+@extends('admin.layouts.app')
+
+@section('main-content')
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Text Editors
+        <small>Advanced form element</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Forms</a></li>
+        <li class="active">Editors</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Title</h3>
+            </div>
+            @include('includes.messages')
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form" action="{{ route('post.update',$post->id) }}" method="post">
+              {{ csrf_field() }}
+              {{ method_field('PUT') }}
+              <div class="box-body">
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <label for="title">Post Title</label>
+                    <input type="text" class="form-control" id="title" name="title" placeholder="Title" value="{{ $post->title }}">
+                  </div>
+                  <div class="form-group">
+                    <label for="subtitle">Post Sub Title</label>
+                    <input type="text" class="form-control" id="subtitle" name="subtitle" placeholder="Sub Title" value="{{ $post->subtitle }}">
+                  </div>
+                  <div class="form-group">
+                    <label for="slug">Post Slug</label>
+                    <input type="text" class="form-control" id="slug" name="slug" placeholder="Slug" value="{{ $post->slug }}">
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="form-group">
+                    <label for="image">File input</label>
+                    <input type="file" id="image" name="image">
+                  </div>
+                  <br>
+                  <br>
+                  <div class="checkbox">
+                    <label>
+                      <input type="checkbox" name="status" @if( $post->status == 1) checked @endif> Publish
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <!-- /.box-body -->
+              <div class="box">
+                <div class="box-header">
+                  <h3 class="box-title">Write Post Body Here
+                    <small>Simple and fast</small>
+                  </h3>
+                </div>
+                <!-- /.box-header -->
+                <div class="box-body pad">
+                    <textarea class="textarea" placeholder="Place some text here" name="body" style="width: 100%; height: 200px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">{{ $post->body }}</textarea>
+                </div>
+              </div>
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <button type="reset" class="btn btn-warning">Reset</button>
+                <a href="{{ route('post.index') }}" class="btn btn-danger">Back</a>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+
+        </div>
+        <!-- /.col-->
+      </div>
+      <!-- ./row -->
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+@endsection
+<!-- jQuery 2.2.3 -->
+<script src="../../plugins/jQuery/jquery-2.2.3.min.js"></script>
+<!-- Bootstrap 3.3.6 -->
+<script src="../../bootstrap/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../../plugins/fastclick/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/app.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../dist/js/demo.js"></script>
+<!-- CK Editor -->
+<script src="https://cdn.ckeditor.com/4.5.7/standard/ckeditor.js"></script>
+<!-- Bootstrap WYSIHTML5 -->
+<script src="../../plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"></script>
+<script>
+  $(function () {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1');
+    //bootstrap WYSIHTML5 - text editor
+    $(".textarea").wysihtml5();
+  });
+</script>
